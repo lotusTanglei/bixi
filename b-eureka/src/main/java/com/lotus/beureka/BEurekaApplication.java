2e9828a53ed7312656b8fdc640e19271eda18b69
tanglei <tanglei_12301@163com>
2022-09-18 21:03:23 +0800

    增加eureka模块，调整项目架构；

--- b-eureka/src/main/java/com/lotus/beureka/BEurekaApplication.java
+++ b-eureka/src/main/java/com/lotus/beureka/BEurekaApplication.java
@@ -1,13 +1,13 @@
-package com.lotus.bixi;
+package com.lotus.beureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BixiApplication {
+public class BEurekaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BixiApplication.class, args);
+		SpringApplication.run(BEurekaApplication.class, args);
 	}
 
 }
