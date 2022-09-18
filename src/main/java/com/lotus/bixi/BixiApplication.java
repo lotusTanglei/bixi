f0f875c136beaa749ba9dba996d330d479d469e7
tanglei <tanglei_12301@163com>
2022-09-18 20:29:21 +0800

    初始化项目；

---
+++ src/main/java/com/lotus/bixi/BixiApplication.java
@@ -0,0 +1,13 @@
+package com.lotus.bixi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BixiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BixiApplication.class, args);
+	}
+
+}
