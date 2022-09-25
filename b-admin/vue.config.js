58d94a4f93c6f0ac5b887ec682949eebba27fe81
tanglei <tanglei_12301@163com>
2022-09-25 20:32:25 +0800

    1、安装ace后台开发模板； 2、整合登录页面；

--- b-admin/vue.config.js
+++ b-admin/vue.config.js
@@ -2,3 +2,15 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+const webpack = require('webpack')
+module.exports = {
+  chainWebpack: config => {
+    config.plugin('provide').use(webpack.ProvidePlugin, [{
+      $: 'jquery',
+      jquery: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery'
+    }])
+  }
+}
